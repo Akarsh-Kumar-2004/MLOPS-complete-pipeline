b6f8d27aaa3e498f72b65695cb0f170e5bd698f4
Akarsh <akarshkumar2004@gmail.com>
2025-12-19 00:14:20 +0530

    basic pipeline made

---
+++ src/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+with Live(save_dvc_exp=True) as live:
+        #     live.log_metric('accuracy', accuracy_score(y_test, y_test))
+        #     live.log_metric('precision', precision_score(y_test, y_test))
+        #     live.log_metric('recall', recall_score(y_test, y_test))
+
+        #     live.log_params(params)
